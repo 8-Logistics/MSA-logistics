9894a0fc50d4edd4dacf38c7e7c230f05a0a1d0d
MINJISU <wltn214400@gmail.com>
2024-12-13 02:49:17 +0900

    [Order]Feat: 주문 생성 기능 구현

--- order/src/main/java/com/logistics/order/presentation/controller/OrderController.java
+++ order/src/main/java/com/logistics/order/presentation/controller/OrderController.java
@@ -1,12 +1,31 @@
 package com.logistics.order.presentation.controller;
 
+import com.logistics.order.application.CustomPrincipal;
+import com.logistics.order.application.dto.ApiResponse;
+import com.logistics.order.application.dto.OrderCreateReqDto;
+import com.logistics.order.application.dto.OrderCreateResDto;
+import com.logistics.order.application.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    /**
+     * 주문 생성
+     */
+    @PostMapping("/orders")
+    public ApiResponse<OrderCreateResDto> createOrder(
+            @RequestBody OrderCreateReqDto request, @AuthenticationPrincipal CustomPrincipal customPrincipal
+    ) {
+        return ApiResponse.success("상품을 등록하였습니다.", orderService.createOrder(request,customPrincipal));
+    }
 }
