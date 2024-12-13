e6d54867522e4f635a82ed8c7d66374521b1497d
pk_hyun <pkh954533@gmail.com>
2024-12-14 00:49:40 +0900

    [Delivery]Feat: queryDsl 설정 추가

---
+++ delivery/src/main/java/com/logistics/delivery/infrastructure/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package com.logistics.delivery.infrastructure.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
