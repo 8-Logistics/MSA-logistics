6b194dbb4828eda0fe836234e1acfc5ee8a1c40b
pk_hyun <pkh954533@gmail.com>
2024-12-13 14:43:53 +0900

    [Delivery]Feat: Entity 구현

---
+++ delivery/src/main/java/com/logistics/delivery/domain/entity/Delivery.java
@@ -0,0 +1,52 @@
+package com.logistics.delivery.domain.entity;
+
+import com.logistics.delivery.domain.common.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "p_delivery")
+public class Delivery extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "delivery_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID orderId; // 주문
+
+    @Column(nullable = false)
+    private UUID sourceHubId; // 출발 허브
+
+    @Column(nullable = false)
+    private UUID destinationHubId; // 도착 허브
+
+    @Column(nullable = false)
+    private String address; // 배송지 주소
+
+    @Column(nullable = false)
+    private String recipientName; // 주문인
+
+    @Column(nullable = false)
+    private String slackId; // 주문자 슬랙
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status; // 배송 상태
+
+    @OneToOne(mappedBy = "delivery", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private DeliveryPath deliveryPath;
+
+    public void addDeliveryPath(DeliveryPath deliveryPath) {
+        this.deliveryPath = deliveryPath;
+        deliveryPath.updateDelivery(this);
+    }
+}
---
+++ delivery/src/main/java/com/logistics/delivery/domain/entity/DeliveryPath.java
@@ -0,0 +1,54 @@
+package com.logistics.delivery.domain.entity;
+
+import com.logistics.delivery.domain.common.BaseEntity;
+import lombok.*;
+
+import jakarta.persistence.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "p_delivery_path")
+public class DeliveryPath extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "delivery_path_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "hub_delivery_manager_id", nullable = false)
+    private UUID hubDeliveryManagerId; // 허브 배송 담당자
+
+    @Column(name = "vendor_delivery_manager_id", nullable = false)
+    private UUID vendorDeliveryManagerId;  //  업체 배송 담당자
+
+    @Column(name = "source_hub_id", nullable = false)
+    private UUID sourceHubId; // 도착허브
+
+    @Column(name = "destination_hub_id", nullable = false)
+    private UUID destinationHubId; // 도착 허브
+
+    @Column(nullable = false)
+    private double distance;  // 예상 거리
+
+    @Column(name = "estimated_time", nullable = false)
+    private LocalTime estimatedTime;   // 예상 시간
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status; // 배송 상태
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id", nullable = false)
+    private Delivery delivery;
+
+    public void updateDelivery(Delivery delivery) {
+        this.delivery = delivery;
+    }
+
+}
