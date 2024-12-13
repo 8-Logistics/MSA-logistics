0b0d2795b41bb6ed9bc2caec49e3ad0ccf35f64b
Yeonju Seo <yeonjuseo712@gmail.com>
2024-12-13 19:32:54 +0900

    [Vendor]Feat: config, security, auditing 설정

---
+++ vendor/src/main/java/com/logistics/vendor/domain/entity/BaseEntity.java
@@ -0,0 +1,58 @@
+package com.logistics.vendor.domain.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false, name = "created_at")
+	private LocalDateTime createdAt;
+
+	@CreatedBy
+	@Column(updatable = false, name = "created_by")
+	private String createdBy;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@LastModifiedBy
+	@Column(name = "updated_by")
+	private String updatedBy;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+
+	@Column(name = "deleted_by")
+	private String deletedBy;
+
+	@Column(name = "is_delete", nullable = false)
+	private boolean isDelete = false;
+
+	public void setDeletedBy(String deletedBy) {
+		this.deletedBy = deletedBy;
+	}
+
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public void setIsDeleted() {
+		this.isDelete = true;
+	}
+
+}
---
+++ vendor/src/main/java/com/logistics/vendor/infrastructure/client/FeignClientInterceptor.java
@@ -0,0 +1,31 @@
+package com.logistics.vendor.infrastructure.client;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.logistics.vendor.application.service.CustomPrincipal;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+	@Override
+	public void apply(RequestTemplate requestTemplate) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		// Java 16부터 도입된 pattern matching 기능이라고 한다. 변수가 자동으로 캐스팅되어 저장된다고 함.
+		if (authentication != null && authentication.getPrincipal() instanceof CustomPrincipal customPrincipal) {
+
+			String username = customPrincipal.getUserId();
+			String role = customPrincipal.getRole();
+
+			requestTemplate.header("X-User-Id", username);
+			requestTemplate.header("X-Roles", role);
+		}
+	}
+}
+
+
