32b65ede6fa5d8c6dfcaf50ed3f725039f9e1f98
pk_hyun <pkh954533@gmail.com>
2024-12-13 16:43:02 +0900

    [Delivery]Feat: 필요한 feignClient 의 dto 구현

---
+++ delivery/src/main/java/com/logistics/delivery/infrastructure/client/dto/HubPathResDto.java
@@ -0,0 +1,18 @@
+package com.logistics.delivery.infrastructure.client.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class HubPathResDto {
+
+    private UUID pathId; // 이동 경로 UUID
+    private UUID sourceHubId; // 출발 허브 UUID
+    private UUID destinationHubId; // 도착 허브 UUID
+    private double distance; // 이동 거리
+    private LocalTime estimatedTime; // 예상 소요 시간
+}
