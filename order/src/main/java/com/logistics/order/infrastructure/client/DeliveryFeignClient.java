9b927bea60ebea332163bf711a9bc5a93b5b4d38
MINJISU <wltn214400@gmail.com>
2024-12-13 21:42:01 +0900

    [Order]Fix: 배송 생성 feign 전달 dto 수정

--- order/src/main/java/com/logistics/order/infrastructure/client/DeliveryFeignClient.java
+++ order/src/main/java/com/logistics/order/infrastructure/client/DeliveryFeignClient.java
@@ -1,7 +1,7 @@
 package com.logistics.order.infrastructure.client;
 
 import com.logistics.order.application.config.FeignConfig;
-import com.logistics.order.application.dto.OrderCreateReqDto;
+import com.logistics.order.application.dto.OrderToDeliveryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,5 +11,5 @@ import java.util.UUID;
 @FeignClient(name = "delivery-service",configuration = FeignConfig.class)
 public interface DeliveryFeignClient {
     @PostMapping("/api/v1/deliveries/createDelivery")
-    public UUID createDelivery(@RequestBody OrderCreateReqDto dto);
+    public UUID createDelivery(@RequestBody OrderToDeliveryDto dto);
 }
