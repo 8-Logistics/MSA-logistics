1d8087ddc2f6100c9f967221fa8a883b5e7a78b9
MINJISU <wltn214400@gmail.com>
2024-12-17 23:06:02 +0900

    Feat: swagger 기능 추가

--- order/src/main/java/com/logistics/order/presentation/controller/OrderController.java
+++ order/src/main/java/com/logistics/order/presentation/controller/OrderController.java
@@ -3,9 +3,10 @@ package com.logistics.order.presentation.controller;
 import com.logistics.order.application.CustomPrincipal;
 import com.logistics.order.application.dto.*;
 import com.logistics.order.application.service.OrderService;
-import com.logistics.order.application.service.SlackService;
 import com.logistics.order.domain.entity.Order;
 import com.querydsl.core.types.Predicate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name="Order", description="Order API")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -27,6 +29,7 @@ public class OrderController {
     /**
      * 주문 생성
      */
+    @Operation(summary="주문 생성", description="새 주문을 생성합니다.")
     @PostMapping("/orders")
     public ApiResponse<OrderCreateResDto> createOrder(
             @RequestBody OrderCreateReqDto request, @AuthenticationPrincipal CustomPrincipal customPrincipal
@@ -37,6 +40,7 @@ public class OrderController {
     /**
      * 주문 단건 조회
      */
+    @Operation(summary="주문 상세 조회", description="특정 주문을 상세 조회합니다.")
     @GetMapping("/orders/{orderId}")
     public ApiResponse<OrderRetrieveResDto> retrieveOrder(@PathVariable("orderId")UUID orderId) {
         return ApiResponse.success("주문이 조회되었습니다.", orderService.retrieveOrder(orderId));
@@ -45,6 +49,7 @@ public class OrderController {
     /**
      * 주문 검색 (전체 조회)
      */
+    @Operation(summary="주문 전체 조회(검색)", description="주문을 전체 조회 or 키워드 검색이 가능합니다.")
     @GetMapping("/orders/search")
     public ApiResponse<OrderSearchResDto> searchProduct(
             @RequestParam(required = false) List<UUID> idList,
